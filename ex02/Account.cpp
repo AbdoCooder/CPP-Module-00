8ca5f998754b2a8ac35ccd4be67809eb803814a4
Abdelkader <abdocooder@gmail.com>
2025-08-23 22:48:03 +0100

    ex02: add Account class skeleton, tests, and sample log

---
+++ ex02/Account.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenajib <abenajib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/23 22:40:03 by abenajib          #+#    #+#             */
+/*   Updated: 2025/08/23 22:42:40 by abenajib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account() {
+	std::cout << "Account constructed" << std::endl;
+}
+
+Account::~Account() {
+	std::cout << "Account destroyed" << std::endl;
+}
