a7b78d5feb04414dae5e3b2c6e2a96d93a0f3c7c
Abdelkader <abdocooder@gmail.com>
2025-08-24 14:28:07 +0100

    Recreate Account.cpp and pass the tests

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,16 +6,116 @@
 /*   By: abenajib <abenajib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 22:40:03 by abenajib          #+#    #+#             */
-/*   Updated: 2025/08/23 22:42:40 by abenajib         ###   ########.fr       */
+/*   Updated: 2025/08/24 14:26:39 by abenajib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <ctime>
 
-Account::Account() {
-	std::cout << "Account constructed" << std::endl;
+// Static member definitions
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+// PRIVATE (declared but not meant to be used outside) default ctor
+Account::Account(void) {}
+
+Account::Account(int initial_deposit) : _accountIndex(_nbAccounts),
+	_amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+{
+	_nbAccounts++;
+	_totalAmount += _amount;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";amount:" << _amount
+			  << ";created"
+	<< std::endl;
 }
 
-Account::~Account() {
-	std::cout << "Account destroyed" << std::endl;
+Account::~Account(void)
+{
+	_nbAccounts--;
+	_totalAmount -= _amount;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";amount:" << _amount
+			  << ";closed"
+	<< std::endl;
+}
+
+void Account::_displayTimestamp(void)
+{
+	std::time_t now = std::time(0);
+	char buf[32];
+	if (std::strftime(buf, sizeof(buf), "[%Y%m%d_%H%M%S]", std::localtime(&now)))
+		std::cout << buf << " ";
+	else
+		std::cout << "[00000000_000000] ";
+}
+
+int Account::getNbAccounts(void) { return _nbAccounts; }
+int Account::getTotalAmount(void) { return _totalAmount; }
+int Account::getNbDeposits(void) { return _totalNbDeposits; }
+int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
+int Account::checkAmount(void) const { return _amount; }
+
+void Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts()
+			  << ";total:" << getTotalAmount()
+			  << ";deposits:" << getNbDeposits()
+			  << ";withdrawals:" << getNbWithdrawals()
+	<< std::endl;
+}
+
+void Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";amount:" << _amount
+			  << ";deposits:" << _nbDeposits
+			  << ";withdrawals:" << _nbWithdrawals
+	<< std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
+	_amount += deposit;
+	std::cout << "index:" << _accountIndex
+			  << ";p_amount:" << (_amount - deposit)
+			  << ";deposit:" << deposit
+			  << ";amount:" << _amount
+			  << ";nb_deposits:" << _nbDeposits
+	<< std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	if (_amount >= withdrawal)
+	{
+		_displayTimestamp();
+		_totalNbWithdrawals++;
+		_nbWithdrawals++;
+		_amount -= withdrawal;
+		std::cout << "index:" << _accountIndex
+				  << ";p_amount:" << (_amount + withdrawal)
+				  << ";withdrawal:" << withdrawal
+				  << ";amount:" << _amount
+				  << ";nb_withdrawals:" << _nbWithdrawals
+		<< std::endl;
+		return true;
+	}
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";p_amount:" << (_amount + withdrawal)
+			  << ";withdrawal:refused"
+	<< std::endl;
+	return false;
 }
--- ex02/tests.cpp
+++ ex02/tests.cpp
@@ -38,28 +38,31 @@ int		main( void ) {
 	ints_t::iterator	wit_end		= withdrawals.end();
 
 	Account::displayAccountsInfos();
+	std::cout << std::endl;
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
-
+	std::cout << std::endl;
 	for ( acc_int_t it( acc_begin, dep_begin );
 		  it.first != acc_end && it.second != dep_end;
 		  ++(it.first), ++(it.second) )
 	{
 		(*(it.first)).makeDeposit( *(it.second) );
 	}
-
+	std::cout << std::endl;
 	Account::displayAccountsInfos();
+	std::cout << std::endl;
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
-
+	std::cout << std::endl;
 	for ( acc_int_t it( acc_begin, wit_begin );
 		  it.first != acc_end && it.second != wit_end;
 		  ++(it.first), ++(it.second) )
 	{
 		(*(it.first)).makeWithdrawal( *(it.second) );
 	}
-
+	std::cout << std::endl;
 	Account::displayAccountsInfos();
+	std::cout << std::endl;
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
-
+	std::cout << std::endl;
 	return 0;
 }
 
